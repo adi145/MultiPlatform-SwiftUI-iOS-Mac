ee7feb40f7f867e0d3f20f7391d2e607b39f9e9b
adi145 <adinarayanaiapp@gmail.com>
2022-02-15 20:46:56 +0700

    Initial Commit

---
+++ Shared/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  Shared
+//
+//  Created by Adinarayana on 15/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
